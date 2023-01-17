a502ba8391d3455cff7e5f05fb11b45a71c6b2ff
3DDanMaddock <dmaddock@thirdera.com>
2023-01-17 13:55:31 -0500

    Working and has words initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "theme-ui";
+import theme from "../lib/foundation/theme";
+import "../lib/foundation/global.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider {...{ theme }}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React, { useLayoutEffect, useRef, useState } from "react";
+import { Box, Container, Input, Text } from "theme-ui";
+import words from "../data/words.json";
 
-const inter = Inter({ subsets: ['latin'] })
+///AIzaSyCmO6gt-EbUixNEigELZfdxAW3UPugRBdM
 
 export default function Home() {
+  const [results, setResults] = useState(null);
+  const definitions = useRef();
+
+  let windowTimeout = null;
+
+  const inputChange = (e) => {
+    console.log(e.target.value);
+
+    definitions.current.classList.add("loading");
+
+    window.clearTimeout(windowTimeout);
+
+    windowTimeout = window.setTimeout(() => {
+      const _value = e.target.value;
+
+      const _results = words.filter((word) => {
+        let include = false;
+
+        if (word.word.includes(_value)) include = true;
+        if (word.definition.includes(_value)) include = true;
+
+        return include;
+      });
+
+      definitions.current.classList.remove("loading");
+
+      setResults(_results);
+    }, 400);
+  };
+
+  useLayoutEffect(() => {
+    console.log(words);
+    setResults(words);
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,110 +49,36 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
+      <Box as="main">
+        <Container className="container" sx={{ textAlign: "center" }}>
+          <Text as="h1" variant="h1" mb="lg">
+            Thirdera ServiceNow Lexicon
+          </Text>
+          <Box as="form" mb="xl" sx={{ textAlign: "left" }}>
+            <Box as="label" sx={{ fontWeight: "bold", pb: "sm", display: "block" }}>
+              Search
+            </Box>
+            <Input onChange={(e) => inputChange(e)} placeholder="Search for..." />
+          </Box>
+          <Box
+            sx={{
+              textAlign: "left",
+              transition: "opacity 0.3s ease-in-out",
+            }}
+            ref={definitions}
           >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+            {results &&
+              results.map((word) => (
+                <Box key={`word-${word.word}`} mb="lg">
+                  <Box sx={{ fontWeight: "bold" }} mb="sm">
+                    {word.word}
+                  </Box>
+                  <Box>{word.definition}</Box>
+                </Box>
+              ))}
+          </Box>
+        </Container>
+      </Box>
     </>
-  )
+  );
 }
